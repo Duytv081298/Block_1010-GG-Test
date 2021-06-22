7a85d2cd6852286e86906da5bbe47a5b6e6fdeed
Duytv081298 <duytv081298@gmail.com>
2021-06-22 09:24:12 +0700

    dddd

--- libs/block.js
+++ libs/block.js
@@ -186,7 +186,7 @@ function setStage() {
 }
 function setBackground() {
     randomN = 1.5
-    console.log('ssssssssssss');
+    console.log('1111111111111');
 
     var bg = new createjs.Sprite(spriteSheet, "bg");
     bg.scaleX = stage.canvas.width / bg.getBounds().width;
